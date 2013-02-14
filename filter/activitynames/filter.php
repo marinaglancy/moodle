9c97f1d202c0f4f1d0a45fb6f6d1c9c901516c97
Marina Glancy <marina@moodle.com>
2013-02-14 17:36:37 +1100

    MDL-31885 Removed static caching from filter_activitynames
    Caching does not save a lot, there are no DB requests in the filter anyway. But it prevents from applying different logic for different modules inside the course

--- filter/activitynames/filter.php
+++ filter/activitynames/filter.php
@@ -31,58 +31,45 @@ defined('MOODLE_INTERNAL') || die();
  * Activity name filtering
  */
 class filter_activitynames extends moodle_text_filter {
-    // Trivial-cache - keyed on $cachedcourseid
-    static $activitylist = null;
-    static $cachedcourseid;
 
     function filter($text, array $options = array()) {
         if (!$courseid = get_courseid_from_context($this->context)) {
             return $text;
         }
 
-        // Initialise/invalidate our trivial cache if dealing with a different course
-        if (!isset(self::$cachedcourseid) || self::$cachedcourseid !== (int)$courseid) {
-            self::$activitylist = null;
-        }
-        self::$cachedcourseid = (int)$courseid;
+        $activitylist = array();
 
-        /// It may be cached
+        $modinfo = get_fast_modinfo($courseid);
+        if (!empty($modinfo->cms)) {
+            $activitylist = array();      /// We will store all the activities here
 
-        if (is_null(self::$activitylist)) {
-            self::$activitylist = array();
+            //Sort modinfo by name length
+            $sortedactivities = fullclone($modinfo->cms);
+            usort($sortedactivities, 'filter_activitynames_comparemodulenamesbylength');
 
-            $modinfo = get_fast_modinfo($courseid);
-            if (!empty($modinfo->cms)) {
-                self::$activitylist = array();      /// We will store all the activities here
-
-                //Sort modinfo by name length
-                $sortedactivities = fullclone($modinfo->cms);
-                usort($sortedactivities, 'filter_activitynames_comparemodulenamesbylength');
-
-                foreach ($sortedactivities as $cm) {
-                    //Exclude current activity, labels, hidden activities and activities for group members only
-                    if (!($this->context->contextlevel == CONTEXT_MODULE && $cm->id == $this->context->instanceid) &&
-                            $cm->visible && empty($cm->groupmembersonly) && $cm->has_view()) {
-                        $title = s(trim(strip_tags($cm->name)));
-                        $currentname = trim($cm->name);
-                        $entitisedname  = s($currentname);
-                        /// Avoid empty or unlinkable activity names
-                        if (!empty($title)) {
-                            $href_tag_begin = html_writer::start_tag('a',
-                                    array('class' => 'autolink', 'title' => $title,
-                                        'href' => $cm->get_url()));
-                            self::$activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
-                            if ($currentname != $entitisedname) { /// If name has some entity (&amp; &quot; &lt; &gt;) add that filter too. MDL-17545
-                                self::$activitylist[] = new filterobject($entitisedname, $href_tag_begin, '</a>', false, true);
-                            }
+            foreach ($sortedactivities as $cm) {
+                //Exclude current activity, labels, hidden activities and activities for group members only
+                if (!($this->context->contextlevel == CONTEXT_MODULE && $cm->id == $this->context->instanceid) &&
+                        $cm->visible && empty($cm->groupmembersonly) && $cm->has_view()) {
+                    $title = s(trim(strip_tags($cm->name)));
+                    $currentname = trim($cm->name);
+                    $entitisedname  = s($currentname);
+                    /// Avoid empty or unlinkable activity names
+                    if (!empty($title)) {
+                        $href_tag_begin = html_writer::start_tag('a',
+                                array('class' => 'autolink', 'title' => $title,
+                                    'href' => $cm->get_url()));
+                        $activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                        if ($currentname != $entitisedname) { /// If name has some entity (&amp; &quot; &lt; &gt;) add that filter too. MDL-17545
+                            $activitylist[] = new filterobject($entitisedname, $href_tag_begin, '</a>', false, true);
                         }
                     }
                 }
             }
         }
 
-        if (self::$activitylist) {
-            return $text = filter_phrases ($text, self::$activitylist);
+        if ($activitylist) {
+            return $text = filter_phrases ($text, $activitylist);
         } else {
             return $text;
         }
