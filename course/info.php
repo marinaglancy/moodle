0c8c38f5f984e55758f1bd78383c3e29149a8689
Marina Glancy <marina@moodle.com>
2013-02-27 16:23:28 +1100

    course/info.php, unfinished

--- course/info.php
+++ course/info.php
@@ -2,60 +2,69 @@
 
 /// Displays external information about a course
 
-    require_once("../config.php");
-    require_once("lib.php");
+require_once("../config.php");
+require_once("lib.php");
 
-    $id   = optional_param('id', false, PARAM_INT); // Course id
-    $name = optional_param('name', false, PARAM_RAW); // Course short name
+$id = optional_param('id', false, PARAM_INT); // Course id
+$name = optional_param('name', false, PARAM_RAW); // Course short name
+$catid = optional_param('catid', false, PARAM_INT); // Course category id
 
+if ($CFG->forcelogin) {
+    require_login();
+}
+
+if ($catid) {
+    require_once($CFG->libdir . '/coursecatlib.php');
+    $category = coursecat::get($catid);
+    $context = context_category::instance($category->id);
+    $PAGE->set_context($context);
+    $PAGE->set_url('/course/info.php', array('catid' => $category->id));
+    $PAGE->set_category_by_id($catid);
+    $PAGE->set_title(get_string("summaryof", "", $category->get_formatted_name()));
+    $PAGE->set_heading(get_string('categoryinfo'));
+} else {
     if (!$id and !$name) {
         print_error("unspecifycourseid");
     }
-
     if ($name) {
-        if (!$course = $DB->get_record("course", array("shortname"=>$name))) {
+        if (!$course = $DB->get_record("course", array("shortname" => $name))) {
             print_error("invalidshortname");
         }
     } else {
-        if (!$course = $DB->get_record("course", array("id"=>$id))) {
+        if (!$course = $DB->get_record("course", array("id" => $id))) {
             print_error("invalidcourseid");
         }
     }
-
-    $site = get_site();
-
-    if ($CFG->forcelogin) {
-        require_login();
-    }
-
     $context = context_course::instance($course->id);
-    if (!$course->visible and !has_capability('moodle/course:viewhiddencourses', $context)) {
-        print_error('coursehidden', '', $CFG->wwwroot .'/');
-    }
-
     $PAGE->set_context($context);
-    $PAGE->set_pagelayout('popup');
+    if (!$course->visible and !has_capability('moodle/course:viewhiddencourses', $context)) {
+        print_error('coursehidden', '', $CFG->wwwroot . '/');
+    }
     $PAGE->set_url('/course/info.php', array('id' => $course->id));
     $PAGE->set_title(get_string("summaryof", "", $course->fullname));
     $PAGE->set_heading(get_string('courseinfo'));
     $PAGE->set_course($course);
-    $PAGE->navbar->add(get_string('summary'));
+}
 
-    echo $OUTPUT->header();
-    echo $OUTPUT->heading('<a href="view.php?id='.$course->id.'">'.format_string($course->fullname) . '</a><br />(' . format_string($course->shortname, true, array('context' => $context)) . ')');
+$site = get_site();
 
-    // print enrol info
-    if ($texts = enrol_get_course_description_texts($course)) {
-        echo $OUTPUT->box_start('generalbox icons');
-        echo implode($texts);
-        echo $OUTPUT->box_end();
-    }
+$PAGE->set_pagelayout('popup');
 
-    $courserenderer = $PAGE->get_renderer('core', 'course');
-    echo $courserenderer->course_info_box($course);
+echo $OUTPUT->header();
+echo $OUTPUT->heading('<a href="view.php?id=' . $course->id . '">' . format_string($course->fullname) . '</a><br />(' . format_string($course->shortname, true, array('context' => $context)) . ')');
 
-    echo "<br />";
+// print enrol info
+if ($texts = enrol_get_course_description_texts($course)) {
+    echo $OUTPUT->box_start('generalbox icons');
+    echo implode($texts);
+    echo $OUTPUT->box_end();
+}
 
-    echo $OUTPUT->footer();
+$courserenderer = $PAGE->get_renderer('core', 'course');
+echo $courserenderer->course_info_box($course);
+
+echo "<br />";
+
+echo $OUTPUT->footer();
 
 
