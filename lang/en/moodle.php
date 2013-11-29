e245a680fb917579e4e456d1c11fc7bab4028046
Rajesh Taneja <rajesh@moodle.com>
2013-11-29 14:13:14 +0800

    MDL-40058 mod_wiki: Replaced add_to_log on index.php with event

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -723,6 +723,7 @@ $string['eventcourseresetstarted'] = 'Course reset started';
 $string['eventcourserestored'] = 'Course restored';
 $string['eventcourseupdated'] = 'Course updated';
 $string['eventcoursesectionupdated'] = ' Course section updated';
+$string['eventinstanceslistviewed'] = 'Instances list viewed';
 $string['eventusercreated'] = 'User created';
 $string['eventuserdeleted'] = 'User deleted';
 $string['eventuserlistviewed'] = 'User list viewed';
