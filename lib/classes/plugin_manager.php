9ef3a3e7946475985f94aa17ca4286110b0fc0fc
Marina Glancy <marina@moodle.com>
2014-03-25 18:03:54 +0800

    MDL-44084 atto: correct standard plugins list and namespace bug fix

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -929,8 +929,12 @@ class core_plugin_manager {
         $standard_plugins = array(
 
             'atto' => array(
-                'bold', 'html', 'indent', 'link', 'orderedlist', 'strike', 'title', 'unlink',
-                'clear', 'image', 'italic', 'media', 'outdent', 'table', 'underline', 'unorderedlist'
+                'accessibilitychecker', 'accessibilityhelper', 'align',
+                'backcolor', 'bold', 'charmap', 'clear', 'collapse', 'emoticon',
+                'equation', 'fontcolor', 'html', 'image', 'indent', 'italic',
+                'link', 'managefiles', 'media', 'noautolink', 'orderedlist',
+                'rtl', 'strike', 'subscript', 'superscript', 'table', 'title',
+                'underline', 'undo', 'unlink', 'unorderedlist'
             ),
 
             'assignment' => array(
@@ -995,7 +999,7 @@ class core_plugin_manager {
             ),
 
             'editor' => array(
-                'textarea', 'tinymce'
+                'atto', 'textarea', 'tinymce'
             ),
 
             'enrol' => array(
