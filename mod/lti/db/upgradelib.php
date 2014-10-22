67fff8247769c6f8f2475016f8784c6b519dc540
Marina Glancy <marina@moodle.com>
2014-10-22 16:02:46 +0800

    MDL-47812 lti: add upgrade script for Oracle
    see also MDL-45843

--- mod/lti/db/upgradelib.php
+++ mod/lti/db/upgradelib.php
@@ -32,14 +32,18 @@ defined('MOODLE_INTERNAL') || die();
 function mod_lti_upgrade_custom_separator() {
     global $DB;
 
+    // Initialise parameter array.
+    $params = array('semicolon' => ';', 'likecr' => "%\r%", 'likelf' => "%\n%", 'lf' => "\n");
+
+    // Initialise NOT LIKE clauses to check for CR and LF characters.
+    $notlikecr = $DB->sql_like('value', ':likecr', true, true, true);
+    $notlikelf = $DB->sql_like('value', ':likelf', true, true, true);
+
+    // Initialise NOT LIKE clauses to check for CR and LF characters.
+    $notlikecr2 = $DB->sql_like('instructorcustomparameters', ':likecr', true, true, true);
+    $notlikelf2 = $DB->sql_like('instructorcustomparameters', ':likelf', true, true, true);
+
     if ($DB->replace_all_text_supported()) {
-
-        // Initialise parameter array.
-        $params = array('semicolon' => ';', 'likecr' => "%\r%", 'likelf' => "%\n%", 'lf' => "\n");
-
-        // Initialise NOT LIKE clauses to check for CR and LF characters.
-        $notlikecr = $DB->sql_like('value', ':likecr', true, true, true);
-        $notlikelf = $DB->sql_like('value', ':likelf', true, true, true);
 
         // Update any instances in the lti_types_config table.
         $sql = 'UPDATE {lti_types_config} ' .
@@ -47,15 +51,31 @@ function mod_lti_upgrade_custom_separator() {
                'WHERE (name = \'customparameters\') AND (' . $notlikecr . ') AND (' . $notlikelf . ')';
         $DB->execute($sql, $params);
 
-        // Initialise NOT LIKE clauses to check for CR and LF characters.
-        $notlikecr = $DB->sql_like('instructorcustomparameters', ':likecr', true, true, true);
-        $notlikelf = $DB->sql_like('instructorcustomparameters', ':likelf', true, true, true);
-
         // Update any instances in the lti table.
         $sql = 'UPDATE {lti} ' .
                'SET instructorcustomparameters = REPLACE(instructorcustomparameters, :semicolon, :lf) ' .
-               'WHERE (instructorcustomparameters IS NOT NULL) AND (' . $notlikecr . ') AND (' . $notlikelf . ')';
+               'WHERE (instructorcustomparameters IS NOT NULL) AND (' . $notlikecr2 . ') AND (' . $notlikelf2 . ')';
         $DB->execute($sql, $params);
+
+    } else {
+
+        // Update any instances in the lti_types_config table.
+        $records = $DB->get_records_sql('SELECT id, value FROM {lti_types_config} ' .
+            'WHERE (name = \'customparameters\') AND (' . $notlikecr . ') AND (' . $notlikelf . ')',
+            $params);
+        foreach ($records as $record) {
+            $record->value = preg_replace('/;/', "\n", $record->value);
+            $DB->update_record('lti_types_config', $record);
+        }
+
+        // Update any instances in the lti table.
+        $records = $DB->get_records_sql('SELECT id, instructorcustomparameters FROM {lti} ' .
+            'WHERE (instructorcustomparameters IS NOT NULL) AND (' . $notlikecr2 . ') AND (' . $notlikelf2 . ')',
+            $params);
+        foreach ($records as $record) {
+            $record->instructorcustomparameters = preg_replace('/;/', "\n", $record->instructorcustomparameters);
+            $DB->update_record('lti', $record);
+        }
 
     }
 
