382b19ebfa8521d454253a1e8fc0b846d22e0bcb
Marina Glancy <marina@moodle.com>
2015-08-07 17:28:13 +0800

    MDL-50817 grades: export custom uppercase fields

--- grade/lib.php
+++ grade/lib.php
@@ -191,7 +191,7 @@ class graded_users_iterator {
                             LEFT JOIN (SELECT * FROM {user_info_data}
                                 WHERE fieldid = :cf$customfieldscount) cf$customfieldscount
                             ON u.id = cf$customfieldscount.userid";
-                    $userfields .= ", cf$customfieldscount.data AS customfield_{$field->shortname}";
+                    $userfields .= ", cf$customfieldscount.data AS customfield_" . core_text::strtolower($field->shortname);
                     $params['cf'.$customfieldscount] = $field->customid;
                     $customfieldscount++;
                 }
@@ -3089,8 +3089,8 @@ abstract class grade_helper {
      */
     public static function get_user_field_value($user, $field) {
         if (!empty($field->customid)) {
-            $fieldname = 'customfield_' . $field->shortname;
-            if (!empty($user->{$fieldname}) || is_numeric($user->{$fieldname})) {
+            $fieldname = 'customfield_' . core_text::strtolower($field->shortname);
+            if (!empty($user->{$fieldname})) {
                 $fieldvalue = $user->{$fieldname};
             } else {
                 $fieldvalue = $field->default;
