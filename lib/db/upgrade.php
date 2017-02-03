f157593ce3e2d3a127c8b875f151c407098b4679
Marina Glancy <marina@moodle.com>
2017-02-03 09:40:46 +0800

    MDL-4782 course: Allow activities in the "stealth" mode
    Add field 'visibleoncoursepage' to the course_modules table
    Add site-wide setting for turning on stealth mode availability
    Add callback for "stealth" mode support in the course formats
    Change display of modules/sections availability on the course page

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2465,5 +2465,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2016122800.00);
     }
 
+    if ($oldversion < 2017020300.00) {
+        // Define field visibleoncoursepage to be added to course_modules.
+        $table = new xmldb_table('course_modules');
+        $field = new xmldb_field('visibleoncoursepage', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '1', 'visible');
+
+        // Conditionally launch add field visibleoncoursepage.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2017020300.00);
+    }
+
     return true;
 }
