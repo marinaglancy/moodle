354eee6526c3ed2eaaa4dc449a092146127a4fc0
Marina Glancy <marina@moodle.com>
2017-05-08 09:20:30 +0800

    MDL-50670 format_weeks: correct sql to work in oracle

--- course/format/weeks/lib.php
+++ course/format/weeks/lib.php
@@ -516,7 +516,8 @@ class format_weeks extends format_base {
 
         // Use one DB query to retrieve necessary fields in course, value for automaticenddate and number of the last
         // section. This query will also validate that the course is indeed in 'weeks' format.
-        $sql = "SELECT c.id, c.format, c.startdate, c.enddate, fo.value AS automaticenddate, MAX(s.section) AS lastsection
+        $insql = $DB->sql_substr('fo.value', 0, 1);
+        $sql = "SELECT c.id, c.format, c.startdate, c.enddate, MAX($insql) AS automaticenddate, MAX(s.section) AS lastsection
                   FROM {course} c
              LEFT JOIN {course_format_options} fo
                     ON fo.courseid = c.id
@@ -527,7 +528,7 @@ class format_weeks extends format_base {
                     ON s.course = c.id
                  WHERE c.format = :format
                    AND c.id = :courseid
-              GROUP BY c.id, c.format, c.startdate, c.enddate, fo.value";
+              GROUP BY c.id, c.format, c.startdate, c.enddate";
         $course = $DB->get_record_sql($sql,
             ['optionname' => 'automaticenddate', 'format' => 'weeks', 'courseid' => $courseid]);
 
