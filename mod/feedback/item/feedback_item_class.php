361dde421d91713dada86658395ac3dae223d27b
Marina Glancy <marina@moodle.com>
2017-05-30 09:45:59 +0800

    MDL-39447 mod_feedback: add old classes with deprecation

---
+++ mod/feedback/item/feedback_item_class.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+debugging('Class feedback_item_base is deprecated, please use mod_feedback_item_base',
+    DEBUG_DEVELOPER);
+
+/**
+ * @deprecated since 3.4
+ */
+abstract class feedback_item_base extends mod_feedback_item_base {
+}
