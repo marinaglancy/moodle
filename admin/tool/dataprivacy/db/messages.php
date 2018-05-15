f4cc9d29a781eb2a5df3a8e55d223277c46f06b3
Marina Glancy <marina@moodle.com>
2018-05-15 12:33:47 +0800

    MDL-62134 tool_dataprivacy: privacy manager wrapper
    If exception occurs in one plugin implementation do not fail the whole job but
    instead send a message to DPOs with the exception details

--- admin/tool/dataprivacy/db/messages.php
+++ admin/tool/dataprivacy/db/messages.php
@@ -41,4 +41,13 @@ $messageproviders = [
             'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
         ]
     ],
+
+    // Notify Data Protection Officer about exceptions.
+    'notifyexceptions' => [
+        'defaults' => [
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
+            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
+        ],
+        'capability'  => 'tool/dataprivacy:managedatarequests'
+    ],
 ];
