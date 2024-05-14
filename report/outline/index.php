ab183a4588a12139df576daebdbd5ca1e95642fa
Marina Glancy <marina@moodle.com>
2024-05-14 22:50:32 +0100

    links

--- report/outline/index.php
+++ report/outline/index.php
@@ -259,7 +259,7 @@ foreach ($modinfo->sections as $sectionnum=>$section) {
             $attributes['class'] = 'dimmed';
         }
 
-        $activitycell->text = $activityicon . html_writer::link("$CFG->wwwroot/mod/$cm->modname/view.php?id=$cm->id", format_string($cm->name), $attributes);
+        $activitycell->text = $activityicon . html_writer::link(new moodle_url("/mod/$cm->modname/view.php?id=$cm->id"), format_string($cm->name), $attributes);
 
         $reportrow->cells[] = $activitycell;
 
